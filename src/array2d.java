1a078b9055cc96f1ca8d3e8c51aeacd7e59901d8
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2022-11-03 14:22:07 -0400

    width height, array2d

--- src/array2d.java
+++ src/array2d.java
@@ -1,10 +1,9 @@
-import java.util.Scanner;
 public class array2d {
     public static void main(String[] args){
-        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        for (int i = 0; i < arr.length; i++){
-            for (int x = 0; x < arr[i].length; x++){
-                System.out.print(arr[i][x] + " ");
+        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        for (int[] ints : arr){
+            for (int i : ints){
+                System.out.print(i + " ");
             }
             System.out.println();
         }
--- src/widthHeight.java
+++ src/widthHeight.java
@@ -4,6 +4,17 @@ public class widthHeight {
         Scanner sc = new Scanner(System.in);
         int width = sc.nextInt();
         int height = sc.nextInt();
-
+        int[][] arr = new int[height][width];
+        for (int i = 0; i<height; i++){
+            for (int j = 0; j < width; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int[] ints : arr) {
+            for (int i : ints) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
     }
 }
