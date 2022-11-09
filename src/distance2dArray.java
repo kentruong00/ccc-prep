c7324242b54eb49d7e818ff0539912017df54512
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2022-11-09 12:44:17 -0500

    started the thing

---
+++ src/distance2dArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class distance2dArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[4];
+        arr[0] = 0;
+        for(int i = 1; i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[][] output = new int[4][4];
+        for(int i = 0; i<4;i++){
+            for(int j = 0;j<4;j++){
+                int distance = 0;
+
+            }
+        }
+    }
+}
