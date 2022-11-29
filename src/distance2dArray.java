837522b346e78e48a67650bbb79d2dcd2bf72211
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2022-11-29 15:14:39 -0500

    started the thing

--- src/distance2dArray.java
+++ src/distance2dArray.java
@@ -2,17 +2,40 @@ import java.util.Scanner;
 public class distance2dArray {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[4];
+        int[] arr = new int[5];
         arr[0] = 0;
-        for(int i = 1; i<arr.length;i++){
+        for(int i = 1; i<5;i++){
             arr[i] = sc.nextInt();
         }
-        int[][] output = new int[4][4];
-        for(int i = 0; i<4;i++){
-            for(int j = 0;j<4;j++){
-                int distance = 0;
+        for (int i : arr){
+            System.out.println(i);
+        }
 
+        int[][] output = new int[5][5];
+        for(int i = 0; i<5;i++){
+            System.out.println(i);
+            for(int j = 0;j<5;j++){
+                int distance = 0;
+                if (i == j){
+                    output[i][j] = distance;
+                } else if (i>j) {
+                    for (int f = i; f>j;f--){
+                        distance += arr[f];
+                    }
+                    output[i][j] = distance;
+                } else {
+                    for (int f = i+1; f<=j;f++){
+                        distance += arr[f];
+                    }
+                    output[i][j] = distance;
+                }
             }
+        }
+        for (int[] ints : output) {
+            for (int i : ints) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
         }
     }
 }
