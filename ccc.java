45ecf9ce3d21d66fb2b15998daf986ce36ca48bf
kentruong00 <93147737+kentruong00@users.noreply.github.com>
2023-02-15 15:13:24 -0500

    sumac

---
+++ ccc.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ccc {
+    public static void main(final String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    System.out.println(a)
+    }
+}
